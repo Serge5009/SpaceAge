63add65733801b6986debb1ccb3723b4fcad2d30
Serhii Marchenko <serge5709244@gmail.com>
2024-03-04 16:06:33 -0500

    UI pulls child orbits from a planet

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,9 +5,10 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    GameObject focusOn;
 
     [SerializeField] TextMeshProUGUI focusNameField;
-    GameObject focusOn;
+    [SerializeField] TMP_Dropdown forwardSelector;
 
     void Start()
     {
@@ -26,7 +27,22 @@ public class UIManager : MonoBehaviour
 
     public void UpdateUI()
     {
+        //  Update data
         focusOn = GameManager.gameManager.focusOn;
+
+        //  Update name
         focusNameField.text = focusOn.GetComponent<Planet>().planetName;
+
+        //  Update forward options
+        forwardSelector.ClearOptions();
+        List<GameObject> childOrbits = focusOn.GetComponent<Planet>().orbits;
+        List<TMP_Dropdown.OptionData> optionsToAdd = new();
+        foreach(GameObject chOrbit in childOrbits)
+        {
+            TMP_Dropdown.OptionData optionData = new(chOrbit.GetComponent<Planet>().planetName);
+            optionsToAdd.Add(optionData);
+        }
+        forwardSelector.AddOptions(optionsToAdd);
+
     }
 }
