b5ecfc06c10311283b58b696c13e5a166a1d00b6
Serhii Marchenko <serge5709244@gmail.com>
2024-03-04 15:36:02 -0500

    GameManager is now a singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance;
+    public static GameManager gameManager   //  Singleton for Game Manager
+    {
+        get
+        {
+            _instance = GameObject.FindObjectOfType<GameManager>();
+            if (_instance == null)
+            {
+                Debug.LogError("No Game Manager found!");
+            }
+            //DontDestroyOnLoad(_instance.gameObject);
+            return _instance;
+        }
+    }
 
     public GameObject focusOn;  //  Space body the camera and UI should focus on
-
+    public UIManager UImanager;
 
     [SerializeField] GameObject camObject;
 
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI focusName;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void UpdateUI()
+    {
+        
+    }
+}
