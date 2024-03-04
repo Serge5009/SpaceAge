f0801753e6149d920acf521e44c6d6ee559398d8
Serhii Marchenko <serge5709244@gmail.com>
2024-03-04 00:34:53 -0500

    Moon spawn on proper distance from the Earth
    *divided in 10

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -10,11 +10,17 @@ public class Planet : MonoBehaviour
 
     public float radius;
 
-    
+    public GameObject orbitAround;
+    public float orbitRadius;
+    public float orbitalVelocity;
 
     void Start()
     {
         transform.localScale = new Vector3(radius / 1000, radius / 1000, radius / 1000);
+        if(orbitAround)
+        {
+            transform.localPosition = orbitAround.transform.position + new Vector3(orbitRadius / 10000, 0 ,0);  //  Orbit distance in the game is 10 times less than real life
+        }
     }
 
     void Update()
