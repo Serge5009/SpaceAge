ee43e93d270f1b5e6bf74eb3c17b95d873aaf038
Serhii Marchenko <serge5709244@gmail.com>
2024-03-04 00:20:36 -0500

    Added planet script

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public string planetName;
+
+    public int population;
+
+    public float radius;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
